46eca4533443e7da0ea5b4e7089bdd232b7a00f6
Diego Bolaños Pardo <37430615+DiegoBP12@users.noreply.github.com>
2018-05-17 23:14:16 -0500

    Tipos_datos
    Primer programa, donde se imprime el valor mínimo y máximo de los tipos de datos primarios numéricos.

---
+++ Tipos_datos.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tipos_datos;
+
+/**
+ *
+ * @author Diego
+ */
+public class Tipos_datos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        byte byte_min= -128;
+        System.out.println("El byte minimo es = " + byte_min);   
+        byte byte_max = 127;
+        System.out.println("El byte máximo es = " + byte_max);
+        
+        short short_min = -32768;
+        System.out.println("El short minimo es = " + short_min);
+        short short_max = 32767;
+        System.out.println("El short máximo es = " + short_max);
+        
+        int int_min = -2147483648;
+        System.out.println("El int minimo es = " + int_min);
+        int int_max = 2147483647;
+        System.out.println("El int minimo es = " + int_max);
+        
+        long long_min = Long.MIN_VALUE;
+        System.out.println("El long minimo es = " + long_min);
+        long long_max = Long.MAX_VALUE;
+        System.out.println("El long minimo es = " + long_max);
+        
+        float float_min = Float.MIN_VALUE;
+        System.out.println("El float minimo es = " + float_min);
+        float float_max = Float.MAX_VALUE;
+        System.out.println("El float minimo es = " + float_max);
+
+        double double_min = Double.MIN_VALUE;
+        System.out.println("El double minimo es = " + double_min);
+        double double_max = Double.MAX_VALUE;
+        System.out.println("El double minimo es = " + double_max);
+        
+        
+        
+
+        
+        
+    } 
+    
+}
